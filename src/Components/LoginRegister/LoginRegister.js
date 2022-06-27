e6122969a9b1a8931f2a48055e87d6753dc9076c
[firstname lastname] <jain.khushi2050@gmail.com>
2022-06-27 16:29:03 +0530

    ok

---
+++ src/Components/LoginRegister/LoginRegister.js
@@ -0,0 +1,28 @@
+/*import React from 'react'
+import './LoginRegister,css'
+function LoginRegister() {
+  const newLocal = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmt_dKoPmX6-0NN1GSK9FbYlmvK_7ASzFHvw&usqp=CAU'
+  return (
+    <>
+      <h3>LOGIN/REGISTER</h3>
+      <center>
+        <ul className="homepageGuide column">
+          <img className="img-responsive" />
+            <p>
+              <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmt_dKoPmX6-0NN1GSK9FbYlmvK_7ASzFHvw&usqp=CAU"} width="100%" alt="example one">
+            </p>
+            <span className="color-overlay"></span>
+        </ul>
+          <ul className="homepageGuide column">
+                <img className="img-responsive"  />
+            <p>
+              <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQfRzJ0jeqATf_JnoPkrJVpx0E5qcj364cTrp7Fi9gt083ZulaBcN_RmQ1--vX2b1H7CEc&usqp=CAU"} width="100%" alt="example two">
+            </p>
+            <span className="color-overlay"></span>
+        </ul>
+      </center>
+    </>
+  )
+}
+
+export default LoginRegister*/
