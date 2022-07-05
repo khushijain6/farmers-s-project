99f639915ed2fbcf13f5036a6faf5ada237eff3b
[firstname lastname] <jain.khushi2050@gmail.com>
2022-07-06 00:50:55 +0530

    responsiveness added

--- src/Components/LoginRegister/LoginRegister.js
+++ src/Components/LoginRegister/LoginRegister.js
@@ -12,20 +12,22 @@ function LoginRegister() {
       </div>
       <div className='image-box'>
         <div className='imagebox'>
-          <ManageAccountsIcon sx={{fontSize:"310px"}}/>
+          <ManageAccountsIcon  sx={{fontSize:{xs:180,sm:250,md:330,lg:340,xl:340}}}/>
           <br/>
-          <Button variant="contained"  sx={{ bgcolor:"#55d483",marginLeft:10,padding: "12px 35px",fontSize:"17px" }} size="large">
-                Click Here
-          </Button>
+          <div className='ok'>
+            <Button variant="contained"  sx={{bgcolor:"#55d483", marginLeft:{xs:2,sm:5,md:10,lg:10,xl:10} ,padding: "12px 35px",fontSize:"17px"}} >
+                  Register 
+            </Button>
+          </div>
         </div>
 
         <div className='imagebox'>
-          <HowToRegIcon sx={{fontSize:"310px"}}/>
+          <HowToRegIcon sx={{fontSize:{xs:180,sm:250,md:330,lg:340,xl:340}}}/>
           <br/>
           <div className='ok'>
-          <Button variant="contained"  sx={{bgcolor:"#55d483", marginLeft:10 ,padding: "12px 35px",fontSize:"17px"}} size="large">
-                Click Here
-          </Button>
+            <Button variant="contained"  sx={{bgcolor:"#55d483", marginLeft:{xs:4,sm:5,md:10,lg:10,xl:10} ,padding: "12px 35px",fontSize:"17px"}} >
+                  Login
+            </Button>
           </div>
        
           
